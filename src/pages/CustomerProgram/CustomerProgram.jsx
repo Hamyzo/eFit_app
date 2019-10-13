d92cb8cce9169eb02a53ddc6ac3e18e8ae91f5f6
yeseniamartinez2 <38821678+yeseniamartinez2@users.noreply.github.com>
2019-10-13 14:45:35 +0200

    Merge branch 'master' of https://gitlab.com/HamDom/eFit_app
    # Conflicts:
    #	.gitignore
    #	.idea/modules.xml
    #	src/App.css
    #	src/App.js

--- src/pages/CustomerProgram/CustomerProgram.jsx
+++ src/pages/CustomerProgram/CustomerProgram.jsx
@@ -5,14 +5,87 @@ import "./CustomerProgram.css";
 class CustomerProgram extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      program: {}
+    };
   }
+
+  componentDidMount() {
+    this.getProgram();
+  }
+
+  getProgram = _ => {
+    fetch(
+      "http://localhost:3009//customerPrograms?_id=5da1f67ccf53670572677651&populate=program"
+    )
+      .then(async response => await response.json())
+      .then(response => this.setState({ program: response }))
+      .catch(err => console.error(err));
+  };
+
+  renderProgram = ({ program }) => (
+    <div key="1">
+      <h1>{program}</h1>
+      <Collapse onChange={this.callback}>
+        <Collapse
+          header={
+            <div className="periodDiv">
+              <Row>
+                <Col span={6}>
+                  <p className="period2">
+                    <strong>Session 1</strong>
+                  </p>
+                </Col>
+                <Col span={6}>
+                  <p className="period2">Status: In Progress</p>
+                </Col>
+                <Col span={6}>
+                  <p className="period2">Start Date: </p>
+                </Col>
+                <Col span={6}>
+                  <p className="period2">End Date: </p>
+                </Col>
+              </Row>
+            </div>
+          }
+          key="1"
+        >
+          <Collapse defaultActiveKey="1">
+            <Collapse
+              header={
+                <div className="periodDiv">
+                  <Row>
+                    <Col span={6}>
+                      <p className="period2">
+                        <strong>Period 1</strong>
+                      </p>
+                    </Col>
+                    <Col span={6}>
+                      <p className="period2">Completed: 2 out of 6 </p>
+                    </Col>
+                    <Col span={6}>
+                      <p className="period2">Start Date: </p>
+                    </Col>
+                    <Col span={6}>
+                      <p className="period2">End Date: </p>
+                    </Col>
+                  </Row>
+                </div>
+              }
+              key="1"
+            ></Collapse>
+          </Collapse>
+        </Collapse>
+      </Collapse>
+    </div>
+  );
 
   callback = key => {
     console.log(key);
   };
 
   render() {
+    const { program } = this.state;
     const text = `
     A dog is a type of domesticated animal.
     Known for its loyalty and faithfulness,
@@ -59,81 +132,14 @@ class CustomerProgram extends React.Component {
     return (
       <div>
         <h1 className="title">Program Title</h1>
-        <p className="description">
+        <p>
           The secret to doing anything is believing that you can do it. Anything
           that you believe you can do strong enough, you can do. Anything. As
           long as you believe. We can fix anything. You are only limited by your
           imagination. That's the way I look when I get home late; black and
           blue. Every highlight needs it's own personal shadow.
         </p>
-        <Collapse onChange={this.callback}>
-          <Panel
-            header={
-              <div className="periodDiv">
-                <Row>
-                  <Col span={6}>
-                    <p className="period2">
-                      <strong>Session 1</strong>
-                    </p>
-                  </Col>
-                  <Col span={6}>
-                    <p className="period2">Status: In Progress</p>
-                  </Col>
-                  <Col span={6}>
-                    <p className="period2">Start Date: </p>
-                  </Col>
-                  <Col span={6}>
-                    <p className="period2">End Date: </p>
-                  </Col>
-                </Row>
-              </div>
-            }
-            key="1"
-          >
-            <Collapse defaultActiveKey="1">
-              <Panel
-                header={
-                  <div className="periodDiv">
-                    <Row>
-                      <Col span={6}>
-                        <p className="period2">
-                          <strong>Period 1</strong>
-                        </p>
-                      </Col>
-                      <Col span={6}>
-                        <p className="period2">Completed: 2 out of 6 </p>
-                      </Col>
-                      <Col span={6}>
-                        <p className="period2">Start Date: </p>
-                      </Col>
-                      <Col span={6}>
-                        <p className="period2">End Date: </p>
-                      </Col>
-                    </Row>
-                  </div>
-                }
-                key="1"
-              >
-                <Table
-                  bordered={false}
-                  pagination={false}
-                  columns={columns}
-                  dataSource={data}
-                  size="middle"
-                />
-              </Panel>
-              <Panel header="Period 2" key="2">
-                <p>{text}</p>
-              </Panel>
-            </Collapse>
-          </Panel>
-          <Panel header="Session 2" key="2">
-            <p>{text}</p>
-          </Panel>
-          <Panel header="Session 3" key="3">
-            <p>{text}</p>
-          </Panel>
-        </Collapse>
+        <div>{this.renderProgram(program)}</div>
       </div>
     );
   }
