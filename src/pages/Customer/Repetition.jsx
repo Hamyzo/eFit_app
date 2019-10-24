2b05dce1890d1463c715fd911a297ff2276e6d5c
Hamza <hamzas.kingdom@gmail.com>
2019-10-24 11:00:51 +0200

    I don't know how this changed lol

--- src/pages/Customer/Repetition.jsx
+++ src/pages/Customer/Repetition.jsx
@@ -14,7 +14,8 @@ const steps = [
         className={"step-img"}
         alt={"Loading"}
         src={
-"https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F71b65044-ce1d-11e7-b1ec-8503a5941b97.jpg?crop=4476%2C2518%2C248%2C647&resize=685"        }
+          "https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2F71b65044-ce1d-11e7-b1ec-8503a5941b97.jpg?crop=4476%2C2518%2C248%2C647&resize=685"
+        }
       />
     )
   },
@@ -25,7 +26,8 @@ const steps = [
         className={"step-img"}
         alt={"Loading"}
         src={
-"http://www.mariadicroce.com/wp-content/uploads/2015/02/workout-at-home.jpg"        }
+          "http://www.mariadicroce.com/wp-content/uploads/2015/02/workout-at-home.jpg"
+        }
       />
     )
   },
@@ -36,7 +38,8 @@ const steps = [
         className={"step-img"}
         alt={"Loading"}
         src={
-"https://d50b62f6164e0c4a0279-11570554cb5edae3285603e6ab25c978.ssl.cf5.rackcdn.com/html_body_blocks/images/000/005/515/original/working_out_at_home_1024x1024_enf0625e2c742e37a36e857417ca769d0f.jpg?1508904721"        }
+          "https://d50b62f6164e0c4a0279-11570554cb5edae3285603e6ab25c978.ssl.cf5.rackcdn.com/html_body_blocks/images/000/005/515/original/working_out_at_home_1024x1024_enf0625e2c742e37a36e857417ca769d0f.jpg?1508904721"
+        }
       />
     )
   },
@@ -47,7 +50,8 @@ const steps = [
         className={"step-img"}
         alt={"Loading"}
         src={
-"http://www.yaduki.com/ss/wp-content/uploads/2018/01/Yoga-Indoors-Downward-Facing-Dog-Pose-532343318_1258x838.jpeg"        }
+          "http://www.yaduki.com/ss/wp-content/uploads/2018/01/Yoga-Indoors-Downward-Facing-Dog-Pose-532343318_1258x838.jpeg"
+        }
       />
     )
   },
@@ -58,7 +62,8 @@ const steps = [
         className={"step-img"}
         alt={"Loading"}
         src={
-"https://s3-ap-northeast-1.amazonaws.com/bhive-jp/media/yogaroom/article/4821/shutterstock_713195971.jpg"        }
+          "https://s3-ap-northeast-1.amazonaws.com/bhive-jp/media/yogaroom/article/4821/shutterstock_713195971.jpg"
+        }
       />
     )
   }
@@ -129,25 +134,30 @@ class Repetition extends React.Component {
   }
 
   render() {
-
     const startCard = (
       <Row>
-      <Col span={24}>
-      <Card
-        style={{  }}
-        cover={
-          <img
-            src={
-              "https://contents.mediadecathlon.com/p1357221/640x0/1000pt1000/2000xcr1500/domyos-cardio-femme.jpg?k=965dd8b4c1b0d95d474cf5b57d1e7663"
+        <Col span={24}>
+          <Card
+            style={{}}
+            cover={
+              <img
+                src={
+                  "https://contents.mediadecathlon.com/p1357221/640x0/1000pt1000/2000xcr1500/domyos-cardio-femme.jpg?k=965dd8b4c1b0d95d474cf5b57d1e7663"
+                }
+              />
             }
-          />
-        }
-      >
-        <Meta title="Program 01" description="This is the description" />
-        <Button block className="btn-start" onClick={() => this.startOnClick()}>
-          START
-        </Button>
-      </Card></Col></Row>
+          >
+            <Meta title="Program 01" description="This is the description" />
+            <Button
+              block
+              className="btn-start"
+              onClick={() => this.startOnClick()}
+            >
+              START
+            </Button>
+          </Card>
+        </Col>
+      </Row>
     );
 
     const modalVisible = this.state.modalVisible;
@@ -197,37 +207,43 @@ class Repetition extends React.Component {
 
         <Row>
           <Col span={24}>
-        <div>
-        <Steps current={this.state.currentStep}>
-          {steps.map(item => (
-            <Step key={item.title} title={item.title} />
-          ))}
-        </Steps>
-        <div className="steps-content">
-          {steps[this.state.currentStep].content}
-          -------- Exercise Description --------
-        </div>
-        <div className="steps-action">
-          {this.state.currentStep < steps.length - 1 && (
-            <Button type="primary" onClick={() => this.showResultModal()}>
-              Done, Next!
-            </Button>
-          )}
-          {this.state.currentStep === steps.length - 1 && (
-            <Button
-              type="primary"
-              //onClick={() => message.success("Processing complete!")}
-              onClick={() => this.handleDoneBtn()}
-            >
-              Done
-            </Button>
-          )}
-          {this.state.currentStep > 0 && (
-            <Button style={{ marginLeft: 8 }} onClick={() => this.prevStep()}>
-              Previous
-            </Button>
-          )} </div>
-        </div></Col></Row>
+            <div>
+              <Steps current={this.state.currentStep}>
+                {steps.map(item => (
+                  <Step key={item.title} title={item.title} />
+                ))}
+              </Steps>
+              <div className="steps-content">
+                {steps[this.state.currentStep].content}
+                -------- Exercise Description --------
+              </div>
+              <div className="steps-action">
+                {this.state.currentStep < steps.length - 1 && (
+                  <Button type="primary" onClick={() => this.showResultModal()}>
+                    Done, Next!
+                  </Button>
+                )}
+                {this.state.currentStep === steps.length - 1 && (
+                  <Button
+                    type="primary"
+                    //onClick={() => message.success("Processing complete!")}
+                    onClick={() => this.handleDoneBtn()}
+                  >
+                    Done
+                  </Button>
+                )}
+                {this.state.currentStep > 0 && (
+                  <Button
+                    style={{ marginLeft: 8 }}
+                    onClick={() => this.prevStep()}
+                  >
+                    Previous
+                  </Button>
+                )}{" "}
+              </div>
+            </div>
+          </Col>
+        </Row>
       </div>
     );
 
