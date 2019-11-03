b2e1eec875989fa34b7a80730afb5ad3486ace5d
Hamza <hamzas.kingdom@gmail.com>
2019-11-03 13:45:15 +0100

    populate exercise

--- src/components/CoachProgram/CoachProgram.jsx
+++ src/components/CoachProgram/CoachProgram.jsx
@@ -21,7 +21,7 @@ class CoachProgram extends React.Component {
         this.props.match
           ? this.props.match.params.customerProgramId
           : "5da1f67ccf53670572677651",
-        "populate=program"
+        "populate=program,sessions.exercises.exercise"
       );
       console.log("Program", program);
       this.setState({ program });
