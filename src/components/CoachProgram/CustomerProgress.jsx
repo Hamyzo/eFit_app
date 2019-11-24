62e7e49b075b69a8ae5409caeb4700e5a522a919
yeseniamartinez2 <38821678+yeseniamartinez2@users.noreply.github.com>
2019-11-24 15:06:30 +0100

    Customer program with focus session progress chart

--- src/components/CoachProgram/CustomerProgress.jsx
+++ src/components/CoachProgram/CustomerProgress.jsx
@@ -248,70 +248,71 @@ class CustomerProgress extends React.Component {
 
   renderProgressChart =(program)=>
   { if (program.focus_sessions.length > 2) {
-    return(<div className="progressChart">
-      <ResponsiveLine
-        curve={select('curve', curveOptions, 'linear')}
-        data={this.exerciseChartData(program.focus_sessions)}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: 'point' }}
-        yScale={{ type: 'linear', stacked: false, min: 'auto', max: 'auto' }}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-          orient: 'bottom',
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'focus session date',
-          legendOffset: 36,
-          legendPosition: 'middle'
-        }}
-        axisLeft={{
-          orient: 'left',
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'count',
-          legendOffset: -40,
-          legendPosition: 'middle'
-        }}
-        colors={{ scheme: 'nivo' }}
-        pointSize={10}
-        pointColor={{ theme: 'background' }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: 'serieColor' }}
-        pointLabel="y"
-        pointLabelYOffset={-12}
-        useMesh={true}
-        legends={[
-          {
-            anchor: 'bottom-right',
-            direction: 'column',
-            justify: false,
-            translateX: 100,
-            translateY: 0,
-            itemsSpacing: 0,
-            itemDirection: 'left-to-right',
-            itemWidth: 80,
-            itemHeight: 20,
-            itemOpacity: 0.75,
-            symbolSize: 12,
-            symbolShape: 'circle',
-            symbolBorderColor: 'rgba(0, 0, 0, .5)',
-            effects: [
-              {
-                on: 'hover',
-                style: {
-                  itemBackground: 'rgba(0, 0, 0, .03)',
-                  itemOpacity: 1
+    return(
+      <div className="progressChart">
+        <h1 className="focusSessionsTitle">Focus Sessions Progress</h1>
+        <ResponsiveLine
+          data={this.exerciseChartData(program.focus_sessions)}
+          margin={{ top: 15, right: 110, bottom: 20, left: 60 }}
+          xScale={{ type: 'point' }}
+          yScale={{ type: 'linear', stacked: false, min: 'auto', max: 'auto' }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            orient: 'bottom',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'focus session date',
+            legendOffset: 36,
+            legendPosition: 'middle'
+          }}
+          axisLeft={{
+            orient: 'left',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'count',
+            legendOffset: -40,
+            legendPosition: 'middle'
+          }}
+          colors={{ scheme: 'nivo' }}
+          pointSize={10}
+          pointColor={{ theme: 'background' }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: 'serieColor' }}
+          pointLabel="y"
+          pointLabelYOffset={-12}
+          useMesh={true}
+          legends={[
+            {
+              anchor: 'bottom-right',
+              direction: 'column',
+              justify: false,
+              translateX: 100,
+              translateY: 0,
+              itemsSpacing: 0,
+              itemDirection: 'left-to-right',
+              itemWidth: 80,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: 'circle',
+              symbolBorderColor: 'rgba(0, 0, 0, .5)',
+              effects: [
+                {
+                  on: 'hover',
+                  style: {
+                    itemBackground: 'rgba(0, 0, 0, .03)',
+                    itemOpacity: 1
+                  }
                 }
-              }
-            ]
-          }
-        ]}
-      />
-    </div>)}
-    else { return ""}
+              ]
+            }
+          ]}
+        />
+      </div>)}
+  else { return ""}
   }
 
 
@@ -320,6 +321,7 @@ class CustomerProgress extends React.Component {
 
     return (
       <div>
+
         {program ? this.renderProgressChart(program) : (
           <Spinner />
         )}
